5713f76e5203be0cbd8e4c19ac1a1bad3745f208
dilina-c <dilinack98@gmail.com>
2023-01-23 00:01:03 +0530

    restless-flower-4947-v4 deployed

--- myproject/MLcode.py
+++ myproject/MLcode.py
@@ -65,4 +65,6 @@ def genarateDataSet():
 
 if __name__ == '__main__':
    app.run(port=8080, debug=False)
-   
+   # http_server = WSGIServer(('', 8080), app)
+   # http_server.serve_forever()
+   #serve(app, host="0.0.0.0", port=8080)
